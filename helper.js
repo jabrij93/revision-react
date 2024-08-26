762db13c79242d5781c97d81a3340c0b0d57f79a
jabss <jabrijuhinin93@gmail.com>
2024-08-26 23:24:11 +0800

    extract format date time function into helper.js

---
+++ helper.js
@@ -0,0 +1,16 @@
+// Helper function to format the date and time
+export const formatDateTime = (datetime) => {
+    const date = new Date(datetime);
+
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+    const year = date.getFullYear();
+
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+
+    const formattedDate = `${day}-${month}-${year}`;
+    const formattedTime = `${hours}:${minutes}`;
+
+    return { formattedDate, formattedTime };
+};
