32e0015223fae4971ce7113e5a6ff9db3fd0322e
jabss <jabrijuhinin93@gmail.com>
2024-08-26 23:24:46 +0800

    city now only displays city

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import axios from 'axios';
+import { formatDateTime } from '../helper.js';
 import './App.css'
 
-const weatherUrl = 'http://worldtimeapi.org/api/timezone/Asia/Jakarta';
+
 
 function App() {
   const [weatherData, setWeatherData] = useState('');
   const [timezone, setTimezone] = useState('');
+  const [city, setCity] = useState('Kuala_Lumpur')
 
   useEffect(() => {
     const getAll = async () => {
+      const weatherUrl = `http://worldtimeapi.org/api/timezone/Asia/${city}`
       try {
         const response = await axios.get(weatherUrl);
         setWeatherData(response.data);
@@ -20,16 +23,32 @@ function App() {
       }
     }
     getAll()
-  }, []);
+  }, [city]);
 
   console.log("weatherData.data", weatherData)
+  // Extract the city name from the timezone
+  const cityName = weatherData.timezone ? weatherData.timezone.split('/')[1] : '';
 
   return (
     <div>
       <p>TEST </p>
-      
-      <div> {weatherData.timezone} </div>
-      <div> Current : {weatherData.datetime} </div>
+
+      <input placeholder='Set a city' value={city} onChange={event => setCity(event.target.value)} />
+     
+      {weatherData ? (
+        
+        <div>
+          <div>City: {cityName} </div>
+          <div>
+            Current Date: {formatDateTime(weatherData.datetime).formattedDate}
+          </div>
+          <div>
+            Current Time: {formatDateTime(weatherData.datetime).formattedTime}
+          </div>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   )
 }
